c29d90b6de75fe0ec7e25a9d8cc7e3742081f0e6
German Martinez <martinez_german@outlook.com>
2021-09-07 22:23:25 -0300

    second commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
 import Lib
+import Data.Matrix
+
+-- Figures
+
+f = [[False,False,False,True,True],[False,False,True,True,False],[False,False,False,True,False],[False,False,False,False,False],[False,False,False,False,False]]
+i = [[False,False,False,False,True],[False,False,False,False,True],[False,False,False,False,True],[False,False,False,False,True],[False,False,False,False,True]]
+l = [[False,False,False,False,True],[False,False,False,False,True],[False,False,False,False,True],[False,False,False,True,True],[False,False,False,False,False]]
+n = [[False,False,False,False,True],[False,False,False,True,True],[False,False,False,True,False],[False,False,False,True,False],[False,False,False,False,False]]
+p = [[False,False,False,False,True],[False,False,False,True,True],[False,False,False,True,True],[False,False,False,False,False],[False,False,False,False,False]]
+t = [[False,False,True,True,True],[False,False,False,True,False],[False,False,False,True,False],[False,False,False,False,False],[False,False,False,False,False]]
+u = [[False,False,True,False,True],[False,False,True,True,True],[False,False,False,False,False],[False,False,False,False,False],[False,False,False,False,False]]
+v = [[False,False,False,False,True],[False,False,False,False,True],[False,False,True,True,True],[False,False,False,False,False],[False,False,False,False,False]]
+w = [[False,False,False,False,True],[False,False,False,True,True],[False,False,True,True,False],[False,False,False,False,False],[False,False,False,False,False]]
+x = [[False,False,False,True,False],[False,False,True,True,True],[False,False,False,True,False],[False,False,False,False,False],[False,False,False,False,False]]
+y = [[False,False,False,True,False],[False,False,False,True,True],[False,False,False,True,False],[False,False,False,True,False],[False,False,False,False,False]]
+z = [[False,False,True,True,False],[False,False,False,True,False],[False,False,False,True,True],[False,False,False,False,False],[False,False,False,False,False]]
+
+figure_set = [f, i, l, n, p, t, u, v, w, x, y, z]
 
 main  = do 
-    print ("PENTONOMINO STACK SOLVER") 
+    print ("PENTONOMINO STACK SOLVER")
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,19 +2,7 @@ module Lib where
 
 import Data.List
 
-empty = [[False,False,False,False,False], [False,False,False,False,False], [False,False,False,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-f = [[False,True,True,False,False], [True,True,False,False,False], [False,True,False,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-i = [[True,False,False,False,False], [True,False,False,False,False], [True,False,False,False,False], [True,False,False,False,False], [True,False,False,False,False]]
-l = [[True,False,False,False,False], [True,False,False,False,False], [True,True,True,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-n = [[False,True,False,False,False], [True,True,False,False,False], [True,False,False,False,False], [True,False,False,False,False], [False,False,False,False,False]]
-p = [[False,True,False,False,False], [True,True,False,False,False], [True,True,False,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-t = [[True,True,True,False,False], [False,True,False,False,False], [False,True,False,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-u = [[True,False,True,False,False], [True,True,True,False,False], [False,False,False,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-v = [[True,False,False,False,False], [True,False,False,False,False], [True,True,True,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-w = [[True,False,False,False,False], [True,True,False,False,False], [False,True,True,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-x = [[False,True,False,False,False], [True,True,True,False,False], [False,True,False,False,False], [False,False,False,False,False], [False,False,False,False,False]]
-y = [[False,True,False,False,False], [True,True,False,False,False], [False,True,False,False,False], [False,True,False,False,False], [False,False,False,False,False]]
-z = [[True,True,False,False,False], [False,True,False,False,False], [False,True,True,False,False], [False,False,False,False,False],[False,False,False,False,False]]
+empty = [[False,False,False,False,False],[False,False,False,False,False],[False,False,False,False,False],[False,False,False,False,False],[False,False,False,False,False]]
 
 rotate_90 = Data.List.transpose . map Data.List.reverse
 
@@ -24,5 +12,34 @@ rotate_270 = rotate_180 . rotate_90
 
 check_row_level_up row_a row_b = Data.List.intersect [True] (zipWith (&&) row_a row_b) == [True]
 
-check_figure_level_up figure_a figure_b =  zipWith (check_row_level_up) figure_a figure_b
+check_figure_level_up figure_a figure_b =  foldl (||) False (zipWith (check_row_level_up) figure_a figure_b)
+
+get_board_height [] = 0
+get_board_height board = length(board)
+sum_figure figure  = foldl (+) 0 (map (length) (map (filter (\ item -> item == True)) figure))
+
+get_board_length [] = 0
+get_board_length (x:xs) = length(x)
+
+move_row_left [] = []
+move_row_left (x:xs) = xs ++ [False]
+
+move_figure_left [] =  []
+move_figure_left figure = map (move_row_left)  figure
+
+-- move_figure_up [] = []
+-- move_figure_up (x:xs) = xs : take (length x) (repeat False)
+
+-- move_all_figure_left :: [[Bool]] -> [[Bool]]
+
+-- move_all_figure_left [] = []
+-- move_all_figure_left empty = empty
+
+pairs [] = []
+
+pairs xs = zip xs (tail xs)
+
+check_pairs figure_list = map (\ (x,y) -> check_figure_level_up x y) (pairs figure_list) k
+
+stack_height figure_list = length (filter (\ x-> x == True) (check_pairs figure_list)) + 1
 
