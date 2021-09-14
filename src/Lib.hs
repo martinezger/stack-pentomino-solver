7b03f557e90e91524f58356ddd1223bd1d2c0ecf
German Martinez <martinez_german@outlook.com>
2021-09-13 22:33:26 -0300

    sum sum

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,13 @@ move_figure_left :: [[Integer]] -> [[Integer]]
 move_figure_left [] =  []
 move_figure_left figure = map (move_row_left)  figure
 
+
+diff_rows :: [Integer] -> [Integer] -> [Integer]
+diff_rows x y = zipWith (-) x y
+
+diff_figure :: [[Integer]] -> [[Integer]] -> [[Integer]]
+diff_figure figure_a figure_b = zipWith (diff_rows) figure_a figure_b
+
 sum_rows :: [Integer] -> [Integer] -> [Integer]
 sum_rows x y = zipWith (+) x y
 
@@ -19,14 +26,14 @@ sum_figure figure_a figure_b = zipWith (sum_rows) figure_a figure_b
 get_max :: [[Integer]] -> Integer
 get_max figure = maximum (map maximum figure)
 
-replace_max :: [Integer] -> [Integer] -> Integer -> [Integer]
-replace_max a b c =  zipWith (\ x y ->  if x == 1 then c else y) a b
+replace_row :: [Integer] -> [Integer] -> Integer -> [Integer]
+replace_row a b c =  zipWith (\ x y ->  if x == 1 then c else y) a b
 
-pile_figures_max :: [[Integer]] -> [[Integer]] -> Integer -> [[Integer]]
-pile_figures_max figure_a figure_b level = zipWith (\ a b -> replace_max a b level) figure_a figure_b
+pile_figures :: [[Integer]] -> [[Integer]] -> Integer -> [[Integer]]
+pile_figures figure_a figure_b level = zipWith (\ a b -> replace_row a b level) figure_a figure_b
 
-replace_not_max :: [Integer] -> [Integer] -> [Integer]
-replace_not_max ant sig = zipWith(\ x y -> if x < y then y else x) ant sig
+sum_sum :: [Integer] -> [Integer] -> [Integer]
+sum_sum a b = zipWith (\ x y ->  if (x + y) > y then x + y else 0) a b
 
-pile_figures_not_max :: [[Integer]] -> [[Integer]] -> [[Integer]]
-pile_figures_not_max figure_a figure_b = zipWith(replace_not_max) figure_a figure_b
+sum_sum_figure :: [[Integer]] -> [[Integer]] -> [[Integer]]
+sum_sum_figure figure_a figure_b = zipWith (sum_sum) figure_a figure_b
